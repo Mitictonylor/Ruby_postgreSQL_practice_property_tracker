2d01393d2284fcbbf8e1fe681387237559de8e48
Antonio Lorusso <mitictonylor@gmail.com>
2020-03-24 17:46:14 +0000

    set up console.rb

--- console.rb
+++ console.rb
@@ -1,1 +1,20 @@
 require('pry-byebug')
+
+
+require_relative('models/property')
+
+house1 = Property.new({ 'numbers_of_bedrooms' => 3,
+                        'year_built' => 1985,
+                        'buy_let' => 'buy',
+                        'build' => 'flat'
+                      });
+house2 = Property.new({ 'numbers_of_bedrooms' => 1,
+                        'year_built' => 2005,
+                        'buy_let' => 'rent',
+                        'build' => 'studio'
+                      });
+house3 = Property.new({ 'numbers_of_bedrooms' => 5,
+                        'year_built' => 2020,
+                        'buy_let' => 'buy',
+                        'build' => 'detached'
+                      });
