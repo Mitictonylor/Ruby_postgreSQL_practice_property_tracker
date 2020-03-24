faadbf34fa870f6bb4c50d2190bdc0cdca5a0b36
Antonio Lorusso <mitictonylor@gmail.com>
2020-03-24 18:11:57 +0000

    define save()

--- console.rb
+++ console.rb
@@ -3,18 +3,24 @@ require('pry-byebug')
 
 require_relative('models/property')
 
-house1 = Property.new({ 'numbers_of_bedrooms' => 3,
+house1 = Property.new({ 'number_of_bedrooms' => 3,
                         'year_built' => 1985,
                         'buy_let' => 'buy',
                         'build' => 'flat'
                       });
-house2 = Property.new({ 'numbers_of_bedrooms' => 1,
+house2 = Property.new({ 'number_of_bedrooms' => 1,
                         'year_built' => 2005,
                         'buy_let' => 'rent',
                         'build' => 'studio'
                       });
-house3 = Property.new({ 'numbers_of_bedrooms' => 5,
+house3 = Property.new({ 'number_of_bedrooms' => 5,
                         'year_built' => 2020,
                         'buy_let' => 'buy',
                         'build' => 'detached'
                       });
+house1.save()
+
+
+
+binding.pry
+nil
--- models/property.rb
+++ models/property.rb
@@ -1,6 +1,6 @@
 require('pg')
 
-Class Property
+class Property
   attr_accessor :number_of_bedrooms, :year_built, :buy_let, :build
   attr_reader :id
   def initialize(houses)
@@ -11,7 +11,22 @@ Class Property
     @id = houses["id"].to_i if houses['id']
   end
 
-
-
-
+  def save()
+    #connect pg to the postgres database name from where it is
+    db = PG.connect({dbname: 'properties', host:'localhost'})
+    #create a variable that will contain the sql command we want to run
+    sql = "INSERT INTO properties
+              (numbers_of_bedrooms, year_built, buy_let, build)
+               VALUES
+               ($1,$2,$3,$4)
+               RETURNING * "
+    #we assign each of the values to the $ to avoid external sql injection
+    values = [@number_of_bedrooms, @year_built, @buy_let, @build]
+    #we associate a string to the command we want to run
+    db.prepare("save", sql)
+    #take the first element of the date (id) and saves it into the instance variable
+    @id = db.exec_prepared("save", values)[0]["id"].to_i
+    #close the connection
+    db.close()
+  end
 end
